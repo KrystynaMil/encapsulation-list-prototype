1b7bdb6e47a4a40c531f2307ad339ba013443d87
KrystynaMil <66964490+KrystynaMil@users.noreply.github.com>
2021-01-22 16:31:23 +0100

    Update list-prototype.js

--- src/list-prototype.js
+++ src/list-prototype.js
@@ -1,5 +1,6 @@
 "use strict";
 
+
 import { logger } from "../../lib/logger.js";
 
 
@@ -67,4 +68,112 @@ export const listPrototype = {
     return div;
   },
 
+  displayTodos: function () {
+
+    var todosUl = document.getElementById(`${this.state.name}ul`);
+    todosUl.innerHTML = "";
+    this.state.todos.forEach(function (todo, position) {
+      const todoLi = document.createElement("li");
+      const todoText = document.createTextNode(todo.text);
+      todoLi.id = position;
+      todoLi.appendChild(todoText);
+      
+      const inputEl = document.createElement("input");
+      inputEl.type = 'checkbox';
+      todoLi.insertBefore(inputEl, todoText);
+      
+      inputEl.addEventListener(
+        "click",
+        this.toggleTodos.bind(this, position)
+      ); 
+      
+      if (this.state.todos[position].completed) {
+        todoLi.className = 'checkbox';
+        inputEl.checked = true;
+      } else {
+        todoLi.classList.remove('checkbox');
+        inputEl.checked = false
+      }
+      
+      //delete button
+
+      var deleteButton = document.createElement("button");
+      deleteButton.innerHTML='<i class="fa fa-trash"></i>';
+      deleteButton.className = "btn";
+      todoLi.appendChild(deleteButton);
+      deleteButton.addEventListener(
+        "click",
+        this.deleteTodos.bind(this, position)
+      );
+      
+      todosUl.appendChild(todoLi);
+    }, this);
+  },
+ 
+
+  addTodos: function (event) {
+    //debugger;
+
+    const uniqueInputId = `${this.state.name}newToDo`;
+
+    let newToDo = document.getElementById(uniqueInputId).value;
+
+    if (newToDo.keyCode === "") {
+      alert("Please write a Todo!");
+      return;
+    }
+    this.state.todos.push({
+      text: newToDo,
+      completed: false,
+    });
+    
+    this.displayTodos();
+    document.getElementById(uniqueInputId).value = "";
+    logger.push({
+      action: "Add Todo",
+      stateName: this.state.name,
+      state: this.state,
+    });
+  },
   
+
+  toggleTodos: function (position) {
+    //debugger
+    if (!this.state.todos[position].completed) {
+      this.state.todos[position].completed = true;
+      this.displayTodos();
+    } else {
+      this.state.todos[position].completed = false;
+      this.displayTodos();
+    }
+    logger.push({
+      action: "Check input",
+      stateName: this.state.todos[position],
+      state: this.state.todos[position].completed,
+    });
+  },
+  
+  toggleAll: function () {
+    //debugger
+    const listObj = this.state.todos;
+    for (let i = 0; i < listObj.length; i++ ) {
+      listObj[i].completed = true;
+    }
+    this.displayTodos();
+  },
+  
+  deleteTodos: function (position) {
+    //debugger;
+
+    this.state.todos.splice(position, 1);
+
+    this.displayTodos();
+
+    logger.push({
+      action: "Delete Todo",
+      stateName: this.state.name,
+      state: this.state,
+    });
+  },
+  
+};
