e6fb4410b1eeecc4c434a7a74194227fb9136b52
KrystynaMil <kristina-manager@hotmail.com>
2021-01-22 16:40:49 +0100

    deleted e-l-p

--- encapsulation-list-prototype/src/init.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import { createNewListHandler } from './handlers/create-new-list.js'
-
-
-document.getElementById('list-name-input')
-  .addEventListener('keyup', createNewListHandler);
